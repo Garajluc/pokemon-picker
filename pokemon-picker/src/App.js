208d90a9931fd5d0dca0e329c6a91148e367450a
Lucia Garajová <lucia.garajova.upa@gmail.com>
2019-10-01 17:58:06 +0200

    display_api_data

--- pokemon-picker/src/App.js
+++ pokemon-picker/src/App.js
@@ -3,9 +3,33 @@ import './App.css';
 
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            items: []
+        }
+    }
+
+    async componentDidMount() {
+        const url = 'https://pokeapi.co/api/v2/pokemon?limit=9';
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({
+            isLoading: false,
+            items: [...data.results]
+        });
+        console.log(this.state.items[0]);
+    }
+
     render() {
         return(
-            <div>Hello</div>
+            <div>
+                {this.state.isLoading ?
+                    <div>loading</div> :
+                    <div>{this.state.items[0].name}</div>}
+            </div>
         );
     }
 }
