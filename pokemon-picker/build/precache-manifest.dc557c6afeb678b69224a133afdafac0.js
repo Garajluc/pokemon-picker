c8454cdbbb45efa837928de272b7477a131b1611
Lucia Garajová <lucia.garajova.upa@gmail.com>
2019-10-03 12:26:32 +0200

    Revert "Revert "update""
    This reverts commit 9ebfcfdd222923069d0fdc29644dfa04a28cbccf.

---
+++ pokemon-picker/build/precache-manifest.dc557c6afeb678b69224a133afdafac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f47c6435b54337f6c49b7744b6d653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9248ebaa552287a9ef2",
+    "url": "/static/css/main.853909ce.chunk.css"
+  },
+  {
+    "revision": "d51acdec8b6a36f2025a",
+    "url": "/static/js/2.ea8bdae4.chunk.js"
+  },
+  {
+    "revision": "e9248ebaa552287a9ef2",
+    "url": "/static/js/main.ed64a9d9.chunk.js"
+  },
+  {
+    "revision": "52081b57a5302e3cf010",
+    "url": "/static/js/runtime-main.afbe6e2e.js"
+  }
+]);
