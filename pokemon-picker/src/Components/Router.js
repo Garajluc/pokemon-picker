48543df2cd5fba17115fbdc8bc8de3c95e40e3c2
Lucia Garajová <lucia.garajova.upa@gmail.com>
2019-10-03 09:33:59 +0200

    Router.js Setup

---
+++ pokemon-picker/src/Components/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import App from '../App';
+import PokemonDetails from "./PokemonDetails/PokemonDetails";
+
+const Router = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={App} exact/>
+            <Route path="/pokemon" component={PokemonDetails}/>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Router;
